747e16e1896168c4dc0501a8f602303f94f869e8
Goldwin <flame.rainHbow@gmail.com>
2024-01-01 18:51:34 +0700

    implement event creation by weekday

--- pkg/events/component.go
+++ pkg/events/component.go
@@ -1,6 +1,8 @@
 package events
 
 import (
+	"time"
+
 	"github.com/Goldwin/ies-pik-cms/pkg/common/out"
 	"github.com/Goldwin/ies-pik-cms/pkg/events/dto"
 )
@@ -9,4 +11,5 @@ type ChurchEventComponent interface {
 	CheckIn(input dto.CheckInEvent, output out.Output[dto.CheckInEvent])
 	SaveEvent(input dto.ChurchEvent, output out.Output[dto.ChurchEvent])
 	SaveEventSchedule(input dto.ChurchEventSchedule, output out.Output[dto.ChurchEventSchedule])
+	CreateDailyEvent(input time.Weekday, output out.Output[[]dto.ChurchEvent])
 }
