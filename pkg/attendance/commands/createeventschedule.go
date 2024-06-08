f0585ecb983b9d3b255bcc03c52bc1d662a9b71d
Hutomo Widjaja <flame.rainHbow@gmail.com>
2024-06-08 17:12:54 +0700

    implement create event schedule command

--- pkg/attendance/commands/createeventschedule.go
+++ pkg/attendance/commands/createeventschedule.go
@@ -1,8 +1,11 @@
 package commands
 
 import (
+	"fmt"
+
 	"github.com/Goldwin/ies-pik-cms/pkg/attendance/entities"
 	. "github.com/Goldwin/ies-pik-cms/pkg/common/commands"
+	"github.com/google/uuid"
 )
 
 const (
@@ -21,11 +24,28 @@ type CreateEventScheduleCommand struct {
 
 func (c CreateEventScheduleCommand) Execute(ctx CommandContext) CommandExecutionResult[entities.EventSchedule] {
 
+	eventSchedule := &entities.EventSchedule{
+		ID:             uuid.NewString(),
+		Name:           c.Name,
+		TimezoneOffset: c.TimezoneOffset,
+		Type:           entities.EventScheduleType(c.ScheduleType),
+	}
+
+	result, err := ctx.EventScheduleRepository().Save(eventSchedule)
+
+	if err != nil {
+
+		return CommandExecutionResult[entities.EventSchedule]{
+			Status: ExecutionStatusFailed,
+			Error: CommandErrorDetail{
+				Code:    CreateEventScheduleCommandErrorAppError,
+				Message: fmt.Sprintf("Failed to Create Event Schedule: %s", err.Error()),
+			},
+		}
+	}
+
 	return CommandExecutionResult[entities.EventSchedule]{
-		Status: ExecutionStatusFailed,
-		Error: CommandErrorDetail{
-			Code:    CreateEventScheduleCommandErrorAppError,
-			Message: "Failed to Create Event Schedule",
-		},
+		Status: ExecutionStatusSuccess,
+		Result: *result,
 	}
 }
