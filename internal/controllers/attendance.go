9f89a69353823bc4fb523442077537b08f3b3ef6
Hutomo Widjaja <flame.rainHbow@gmail.com>
2024-06-25 20:20:45 +0700

    [WIP] add skeleton for attendance controller

--- internal/controllers/attendance.go
+++ internal/controllers/attendance.go
@@ -22,17 +22,17 @@ func InitializeAttendanceController(r *gin.Engine, middleware middleware.Middlew
 	rg.POST("schedules", attendanceController.createSchedule)
 	
 	scheduleURL := "schedules/:id"
-	rg.GET(scheduleURL)
-	rg.PUT(scheduleURL)
-	rg.DELETE(scheduleURL)
+	rg.GET(scheduleURL, attendanceController.getEventSchedule)
+	rg.PUT(scheduleURL, attendanceController.updateEventSchedule)
+	rg.DELETE(scheduleURL, attendanceController.deleteEventSchedule)
 
-	rg.GET("schedules/:scheduleID/events")
-	rg.GET("schedules/:scheduleID/events/:eventID")
+	rg.GET("schedules/:scheduleID/events", attendanceController.listEventsBySchedule)
+	rg.GET("schedules/:scheduleID/events/:eventID", attendanceController.getEventBySchedule)
 
-	rg.GET("schedules/:scheduleID/events/:eventID/checkin")
-	rg.POST("schedules/:scheduleID/events/:eventID/checkin")
+	rg.GET("schedules/:scheduleID/events/:eventID/checkin", attendanceController.listEventCheckIn)
+	rg.POST("schedules/:scheduleID/events/:eventID/checkin", attendanceController.checkIn)
 
-	rg.GET("schedules/:scheduleID/stats")
+	rg.GET("schedules/:scheduleID/stats", attendanceController.getEventScheduleStats)
 }
 
 func (a *attendanceController) createSchedule(c *gin.Context) {
@@ -52,3 +52,52 @@ func (a *attendanceController) createSchedule(c *gin.Context) {
 	}
 	a.attendanceComponent.CreateEventSchedule(c, schedule, output)
 }
+
+func (a *attendanceController) getEventSchedule(c *gin.Context) {
+	//TODO fill this
+	// id := c.Param("id")
+	// output := &outputDecorator[attendance.EventScheduleDTO]{
+	// 	output: nil,
+	// 	errFunction: func(err out.AppErrorDetail) {
+	// 		c.JSON(400, gin.H{
+	// 			"error": err,
+	// 		})
+	// 	},
+	// 	successFunc: func(result attendance.EventScheduleDTO) {
+	// 		c.JSON(200, gin.H{
+	// 			"data": result,
+	// 		})
+	// 	},
+	// }
+	// //a.attendanceComponent.GetEventSchedule(c, id, output)
+}
+
+func (a *attendanceController) updateEventSchedule(c *gin.Context) {
+	//TODO fill this
+}
+
+func (a *attendanceController) deleteEventSchedule(c *gin.Context) {
+	//TODO fill this
+}	
+
+func (a *attendanceController) getEventScheduleStats(c *gin.Context) {
+	//TODO fill this
+}
+
+func (a *attendanceController) listEventsBySchedule(c *gin.Context) {
+	//TODO fill this
+}	
+
+func (a *attendanceController) getEventBySchedule(c *gin.Context) {
+	//TODO fill this
+}
+
+func (a *attendanceController) listEventCheckIn(c *gin.Context) {
+	//TODO fill this
+}
+
+func (a *attendanceController) checkIn(c *gin.Context) {
+	//TODO fill this
+}
+
+
