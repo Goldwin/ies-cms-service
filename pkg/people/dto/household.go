d609fcde3966aa85f86bad066099fd8d1aded919
Goldwin <flame.rainHbow@gmail.com>
2024-09-14 00:20:57 +0700

    update json to camel case convention

--- pkg/people/dto/household.go
+++ pkg/people/dto/household.go
@@ -2,25 +2,25 @@ package dto
 
 type HouseholdPerson struct {
 	ID                string `json:"id"`
-	FirstName         string `json:"first_name"`
-	MiddleName        string `json:"middle_name"`
-	LastName          string `json:"last_name"`
-	PhoneNumber       string `json:"phone_number"`
-	EmailAddress      string `json:"email_address"`
-	ProfilePictureUrl string `json:"profile_picture_url"`
+	FirstName         string `json:"firstName"`
+	MiddleName        string `json:"middleName"`
+	LastName          string `json:"lastName"`
+	PhoneNumber       string `json:"phoneNumber"`
+	EmailAddress      string `json:"emailAddress"`
+	ProfilePictureUrl string `json:"profilePictureUrl"`
 }
 
 type Household struct {
 	ID            string            `json:"id"`
 	Name          string            `json:"name"`
-	PictureUrl    string            `json:"picture_url"`
-	HouseholdHead HouseholdPerson   `json:"household_head"`
+	PictureUrl    string            `json:"pictureUrl"`
+	HouseholdHead HouseholdPerson   `json:"householdHead"`
 	Members       []HouseholdPerson `json:"members"`
 }
 
 type HouseHoldInput struct {
 	ID                    string   `json:"id"`
 	Name                  string   `json:"name"`
-	HouseholdHeadPersonId string   `json:"head_person_id"`
-	MemberPersonsIds      []string `json:"member_person_ids"`
+	HouseholdHeadPersonId string   `json:"headPersonId"`
+	MemberPersonsIds      []string `json:"memberPersonIds"`
 }
