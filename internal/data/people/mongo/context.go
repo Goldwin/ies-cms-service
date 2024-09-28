75c39c6d8ea7da150d05efb7348f0f7487949e61
Goldwin <flame.rainHbow@gmail.com>
2024-09-28 14:10:44 +0700

    implement lazy loading for people repository

--- internal/data/people/mongo/context.go
+++ internal/data/people/mongo/context.go
@@ -10,6 +10,8 @@ import (
 )
 
 type commandContextImpl struct {
+	ctx                 context.Context
+	db                  *mongo.Database
 	householdRepository repositories.HouseholdRepository
 	personRepository    repositories.PersonRepository
 }
@@ -31,38 +33,56 @@ func (q *queryContextImpl) SearchHousehold() queries.SearchHousehold {
 
 // ViewPersonByEmail implements queries.QueryContext.
 func (q *queryContextImpl) ViewPersonByEmail() queries.ViewPersonByEmail {
+	if q.viewPersonByEmail == nil {
+		q.viewPersonByEmail = ViewPersonByEmail(q.ctx, q.db)
+	}
 	return q.viewPersonByEmail
 }
 
 // ViewHouseholdByPerson implements queries.QueryContext.
 func (q *queryContextImpl) ViewHouseholdByPerson() queries.ViewHouseholdByPerson {
+	if q.viewHouseholdByPerson == nil {
+		q.viewHouseholdByPerson = ViewHouseholdByPerson(q.ctx, q.db)
+	}
 	return q.viewHouseholdByPerson
 }
 
 // SearchPerson implements queries.QueryContext.
 func (q *queryContextImpl) SearchPerson() queries.SearchPerson {
+	if q.searchPerson == nil {
+		q.searchPerson = SearchPerson(q.ctx, q.db)
+	}
 	return q.searchPerson
 }
 
 // ViewPerson implements queries.QueryContext.
 func (q *queryContextImpl) ViewPerson() queries.ViewPerson {
+	if q.viewPerson == nil {
+		q.viewPerson = ViewPerson(q.ctx, q.db)
+	}
 	return q.viewPerson
 }
 
 // HouseholdRepository implements repositories.Context.
 func (c *commandContextImpl) HouseholdRepository() repositories.HouseholdRepository {
+	if c.householdRepository == nil {
+		c.householdRepository = NewHouseholdRepository(c.ctx, c.db)
+	}
 	return c.householdRepository
 }
 
 // PersonRepository implements repositories.Context.
 func (c *commandContextImpl) PersonRepository() repositories.PersonRepository {
+	if c.personRepository == nil {
+		c.personRepository = NewPersonRepository(c.ctx, c.db)
+	}
 	return c.personRepository
 }
 
 func NewCommandContext(ctx context.Context, db *mongo.Database) commands.CommandContext {
 	return &commandContextImpl{
-		householdRepository: NewHouseholdRepository(ctx, db),
-		personRepository:    NewPersonRepository(ctx, db),
+		ctx:                 ctx,
+		db:                  db,
 	}
 }
 
